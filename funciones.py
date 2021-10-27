dff7fe94474f339ee4c99af8127bd5585a7982e0
Alejandro Jerez Colfenix GPS <jerez1.2.3.102107@gmail.com>
2021-10-27 15:08:14 -0500

    docstrings --> funciones.py(suma)

--- funciones.py
+++ funciones.py
@@ -3,6 +3,12 @@
 #    return <expresion>
 
 def suma(a, b):
+    """ Suma dos valores a y b.
+    param int a cualquier entero
+    param int b cualquier entero
+    returns la sumatoria de a y b
+    """
+
     total = a + b
     return total
 
