3014dc2060e8f0df65b28f2a9a68f4b30511b3b3
Alejandro Jerez Colfenix GPS <jerez1.2.3.102107@gmail.com>
2021-10-27 12:03:58 -0500

    iteraciones for

--- iteraciones.py
+++ iteraciones.py
@@ -8,6 +8,8 @@
 contador_externo = 0
 contador_interno = 0
 
+print('iteracion while bucles indefinidos')
+
 while contador_externo < 5:
     while contador_interno < 6:
         print(contador_externo, contador_interno)
@@ -18,3 +20,39 @@ while contador_externo < 5:
 
     contador_externo += 1
     contador_interno = 0
+
+
+# for <variable> in <iterable>:
+#    <expresion>
+
+
+# En la definición anterior debemos entender <iterable> como una colección de objetos;
+# y la <variable> como el elemento específico que se está exponiendo mediante el bucle en
+# cada iteración.
+
+frutas = ['manzana', 'pera', 'mango']
+
+
+print('iteracion for bucles definidos')
+
+for fruta in frutas:
+    print(fruta)
+
+
+# estudiantes = {
+#     'mexico': 10,
+#     'colombia': 15,
+#     'puerto_rico': 4,
+# }
+
+# for pais in estudiantes:
+#     ...
+
+# for pais in estudiantes.keys():
+#     ...
+
+# for numero_de_estudiantes in estudiantes.values():
+#     ...
+
+# for pais, numero_de_estudiantes in estudiantes.items():
+#     ...
